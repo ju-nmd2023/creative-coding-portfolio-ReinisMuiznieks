7513b5435ec0b29c825fad569b10bc84aeb5801d
ReinisMuiznieks <muizreinis@gmail.com>
2025-09-06 23:21:13 +0700

    feat(gravel_disorder): add disorder file

---
+++ experiments/gravel/gravel_disorder.js
@@ -0,0 +1,33 @@
+const squareSize = 40;
+const rows = floor(height / squareSize);
+const cols = floor(width / squareSize);
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  noLoop();
+  textAlign(CENTER, CENTER);
+  textSize(32);
+  background(0);
+}
+
+function draw() {
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      const maxDisplacement = row * 0.9;
+      const maxRotation = radians(row * 4);
+      const x = col * squareSize + squareSize / 2;
+      const y = row * squareSize + squareSize / 2;
+
+      const offsetX = random(-maxDisplacement, maxDisplacement);
+      const offsetY = random(-maxDisplacement, maxDisplacement);
+
+      push();
+      translate(x + offsetX, y + offsetY);
+      rotate(random(-maxRotation, maxRotation));
+      scale(map(row + col, 0, rows + cols - 2, 0.6, 3));
+      fill(255);
+      text("A", 0, 0);
+      pop();
+    }
+  }
+}
